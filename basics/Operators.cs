ac9941ece6ddd45c296848187c4c0cce944d0910
Ahmed Hamdy <ahmansyyoussef@hotmail.com>
2026-01-15 16:28:03 +0200

    your message here

---
+++ basics/Operators.cs
@@ -0,0 +1,24 @@
+using System;
+namespace CSharp
+{
+    class Operators
+    {
+        public static void Run()
+        {
+        /*
+
+         * Operators are divided into:
+         * 1. Arithmetic Operators: +, -, *, /, %, ++, -- are used for mathematical calculations.
+         * 2. Comparison Operators: ==, !=, >, <, >=, <= -- used to compare two values.
+         * 3. Logical Operators: &&, ||, !   -- used to combine multiple boolean expressions.
+         * 4. Assignment Operators: =, +=, -=, *=, /=, %= -- used to assign values to variables.
+         * 5. Bitwise Operators: &, |, ^, ~, <<, >>  -- used to perform operations on binary representations of numbers.
+         * 6. miscellaneous Operators: sizeof, typeof, is, as -- used for various specific operations.
+         
+
+
+
+        */
+        }
+    }
+}
